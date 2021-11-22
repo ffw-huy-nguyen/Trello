3fd467bf7881d7ecd1b8d7241277c1ca7fac830c
huy nguyen <huy.nguyen@ffwagency.com>
2021-11-22 14:15:42 +0700

    Setup react, tailwind and eslint.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1 className="text-blue-300">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
